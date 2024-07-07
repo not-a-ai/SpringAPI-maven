e70d4f53732032c34be9d3fa9056e18403cb8c36
Aléxia <alexia.cassia09@gmail.com>
2024-07-07 17:22:49 -0400

    configurando dockerfile

--- src/main/java/dio/java/Application.java
+++ src/main/java/dio/java/Application.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-	public String PORT = System.getenv("PORT");
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
